cd0494e960a94a2b91be10a1ec83698fae1e3bd8
Blanxxty <Mvtvr21@yandex.ru>
2024-06-24 21:33:00 +0300

    Initial Commit

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Марат Хасанов on 24.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
